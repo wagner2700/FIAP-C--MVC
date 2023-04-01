ff50c626bebe1972f9d9dcbb45d903bcdc603380
wagner2700 <waguinhoxd4@gmail.com>
2023-04-01 20:18:51 -0300

    Adicionar arquivos de projeto.

---
+++ Fiap.Web.ASPNET/Models/ClienteModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Fiap.Web.ASPNET.Models
+{
+    public class ClienteModel
+    {
+        public int ClienteId { get; set; }
+        public string Nome{ get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Observacao{ get; set; }
+
+
+        public int RepresentanteId{ get; set; }
+        public RepresentanteModel representante{ get; set; }
+    }
+}
---
+++ Fiap.Web.ASPNET/Models/RepresentanteModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Fiap.Web.ASPNET.Models
+{
+    public class RepresentanteModel
+    {
+        public int RpresentanteId { get; set; }
+        public string  NomeRepresentante { get; set; }
+        public string Cpf { get; set; }
+        public string Token { get; set; }
+
+        public RepresentanteModel(int rpresentanteId, string nomeRepresentante)
+        {
+            RpresentanteId = rpresentanteId;
+            NomeRepresentante = nomeRepresentante;
+        }
+
+       
+        public RepresentanteModel()
+        {
+
+        }
+
+        public RepresentanteModel(int rpresentanteId, string nomeRepresentante, string cpf) : this(rpresentanteId, nomeRepresentante)
+        {
+            Cpf = cpf;
+        }
+    }
+}
